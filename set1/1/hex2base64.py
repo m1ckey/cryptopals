d4f46360c033b45bd2b99bb29fba3a4a02c2b120
m1ckey <ch.michael.krickl@gmail.com>
2019-02-06 23:36:02 +0100

    add padding when input is uneven

--- set1/1/hex2base64.py
+++ set1/1/hex2base64.py
@@ -7,6 +7,9 @@ hex = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6
 if len(sys.argv) == 2:
     hex = sys.argv[1]
 
+if (len(hex) % 2) == 1:
+    hex = '0' + hex
+
 bytes = bytearray.fromhex(hex)
 b64 = base64.b64encode(bytes).decode()
 
