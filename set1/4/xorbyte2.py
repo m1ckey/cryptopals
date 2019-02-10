c8c5e52c11c69ce25cb700de5891b7122aef697b
Lena Heimberger <lena.heimberger@student.tugraz.at>
2019-02-10 17:26:50 +0100

    prettier format

--- set1/4/xorbyte2.py
+++ set1/4/xorbyte2.py
@@ -1,17 +1,17 @@
+import library
 import sys
 sys.path.insert(0, '../')
-import library
 
 if __name__ == '__main__':
-    strings=open('set.txt', 'r').read()
-    strings=filter(None, strings.split('\n')) # delete empty strings
-    for string in strings: 
+    strings = open('set.txt', 'r').read()
+    strings = filter(None, strings.split('\n'))  # delete empty strings
+    for string in strings:
         result = library.brute(string)
-        decoded=False
+        decoded = False
         for i, r in enumerate(result):
             if library.is_printable(r):
-                decoded=True
+                decoded = True
                 print('%02x : %s' % (i, r.decode()))
-        if decoded==True: 
+        if decoded:
             print('Result for %s \n' % string)
-        decoded=False
+        decoded = False
